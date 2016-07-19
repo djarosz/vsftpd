8737463936ffe7d24f637896d96bfd3149f0ece4
Dawid Jarosz <dawid.jarosz@gmail.com>
2016-07-19 18:57:12 +0200

    3.0.3.5-debian version

--- vsftpver.h
+++ vsftpver.h
@@ -1,7 +1,7 @@
 #ifndef VSF_VERSION_H
 #define VSF_VERSION_H
 
-#define VSF_VERSION "3.0.3.4-debian-patches (https://github.com/djarosz/vsftpd)"
+#define VSF_VERSION "3.0.3.5-debian-patches (https://github.com/djarosz/vsftpd)"
 
 #endif /* VSF_VERSION_H */
 
